f6e07727be63ab988041d6f863d076e0f403c2ba
jimin <gmlwo7819@gmail.com>
2023-03-07 13:45:05 +0900

    first commit

---
+++ lib/screens/onboding/onboding_screen.dart
@@ -0,0 +1,445 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:rive/rive.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  late RiveAnimationController _btnAnimationColtroller;
+
+  @override
+  void initState() {
+    _btnAnimationColtroller = OneShotAnimation(
+      "active",
+      autoplay: false,
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            bottom: 200,
+            left: 100,
+            child: Image.asset("assets/Backgrounds/Spline.png"),
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+            ),
+          ),
+          const RiveAnimation.asset("assets/RiveAssets/shapes.riv"),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: const SizedBox(),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(),
+                  SizedBox(
+                    width: 260,
+                    child: Column(
+                      children: const [
+                        Text(
+                          "Learn design & code",
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontFamily: "Poppins",
+                            height: 1.2,
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          "Don’t skip design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools.",
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(flex: 3),
+
+                  // TODO: Add the animated Button
+                  GestureDetector(
+                    onTap: () {
+                      Future.delayed(
+                        const Duration(milliseconds: 800),
+                        () {
+                          // TODO: Update dialog status
+
+                          customSigninDialog(context, onCLosed: (_) {});
+                        },
+                      );
+                      // TODO: Start the animation
+                      _btnAnimationColtroller.isActive = true;
+                    },
+                    child: SizedBox(
+                      height: 64,
+                      width: 260,
+                      child: Stack(
+                        children: [
+                          RiveAnimation.asset(
+                            "assets/RiveAssets/button.riv",
+                            // TODO: Add Rive Controller
+                            controllers: [_btnAnimationColtroller],
+                          ),
+                          Positioned.fill(
+                            top: 8,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Icon(CupertinoIcons.arrow_right),
+                                SizedBox(width: 8),
+                                Text(
+                                  "Start the course",
+                                  style: TextStyle(fontWeight: FontWeight.w600),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 24),
+                    child: Text(
+                      "Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Future<Object?> customSigninDialog(BuildContext context,
+    {required ValueChanged onCLosed}) {
+  return showGeneralDialog(
+    barrierDismissible: true,
+    barrierLabel: "Sign In",
+    context: context,
+    // TODO: Custom transition
+    pageBuilder: (context, _, __) => Center(
+      child: Container(
+        height: 620,
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.94),
+          borderRadius: const BorderRadius.all(Radius.circular(40)),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Column(
+                children: [
+                  // TODO: Sign in text and decsription
+                  const Text(
+                    "Sign In",
+                    style: TextStyle(fontSize: 34, fontFamily: "Poppins"),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    child: Text(
+                      "Access to 240+ hours of content. Learn design and code, by building real apps with Flutter and Swift.",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+
+                  // TODO: Sign in form
+                  const SignInForm(),
+
+                  // TODO: Divider
+                  Row(
+                    children: const [
+                      Expanded(child: Divider()),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(
+                          "OR",
+                          style: TextStyle(
+                            color: Colors.black26,
+                          ),
+                        ),
+                      ),
+                      Expanded(child: Divider()),
+                    ],
+                  ),
+
+                  // TODO: Socal login
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/email_box.svg",
+                          height: 64,
+                          width: 64,
+                        ),
+                      ),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/apple_box.svg",
+                          height: 64,
+                          width: 64,
+                        ),
+                      ),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/google_box.svg",
+                          height: 64,
+                          width: 64,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              // TODO: Close button
+            ],
+          ),
+        ),
+      ),
+    ),
+  ).then(onCLosed);
+}
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool isShowLoading = false;
+  bool isShowConfetti = false;
+
+  late SMITrigger confetti;
+
+  late SMITrigger check;
+  late SMITrigger error;
+  late SMITrigger reset;
+
+  void signIn(BuildContext context) {
+    setState(() {
+      isShowLoading = true;
+      // TODO: Show confetti aniamton
+      isShowConfetti = true;
+    });
+    Future.delayed(
+      const Duration(seconds: 1),
+      () {
+        if (_formKey.currentState!.validate()) {
+          check.fire();
+          Future.delayed(
+            const Duration(seconds: 2),
+            () {
+              setState(() {
+                isShowLoading = false;
+              });
+              // TODO: Fire confetti animation
+              confetti.fire();
+
+              // TODO: Navigate to next screen
+            },
+          );
+        } else {
+          error.fire();
+          Future.delayed(
+            const Duration(seconds: 2),
+            () {
+              setState(() {
+                isShowLoading = false;
+              });
+            },
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Email",
+                style: TextStyle(color: Colors.black54),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 16),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "";
+                    }
+                    return null;
+                  },
+                  onSaved: (email) {},
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: SvgPicture.asset("assets/icons/email.svg"),
+                    ),
+                  ),
+                ),
+              ),
+              const Text(
+                "Password",
+                style: TextStyle(color: Colors.black54),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 16),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "";
+                    }
+                    return null;
+                  },
+                  onSaved: (password) {},
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: SvgPicture.asset("assets/icons/password.svg"),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 24),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    // TODO: Show the loading animation
+                    signIn(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFF77D8E),
+                    minimumSize: const Size(double.infinity, 56),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(25),
+                        bottomRight: Radius.circular(25),
+                        bottomLeft: Radius.circular(25),
+                      ),
+                    ),
+                  ),
+                  icon: const Icon(
+                    CupertinoIcons.arrow_right,
+                    color: Color(0xFFFE0037),
+                  ),
+                  label: const Text("Sign In"),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        // TODO: Loading animation
+        isShowLoading
+            ? CustomPositioned(
+                child: RiveAnimation.asset(
+                  "assets/RiveAssets/check.riv",
+                  onInit: (artboard) {
+                    //  TODO: Set the trigger
+                    StateMachineController? controller =
+                        StateMachineController.fromArtboard(
+                            artboard, "State Machine 1");
+                    artboard.addController(controller!);
+                    check = controller.findSMI("Check") as SMITrigger;
+                    error = controller.findSMI("Error") as SMITrigger;
+                    reset = controller.findSMI("Reset") as SMITrigger;
+                  },
+                ),
+              )
+            : const SizedBox(),
+
+        // TODO: Confetti animation
+        isShowConfetti
+            ? CustomPositioned(
+                child: Transform.scale(
+                  scale: 7,
+                  child: RiveAnimation.asset(
+                    "assets/RiveAssets/confetti.riv",
+                    onInit: (artboard) {
+                      StateMachineController? controller =
+                          StateMachineController.fromArtboard(
+                              artboard, "State Machine 1");
+                      artboard.addController(controller!);
+                      confetti =
+                          controller.findSMI("Trigger explosion") as SMITrigger;
+                    },
+                  ),
+                ),
+              )
+            : const SizedBox(),
+      ],
+    );
+  }
+}
+
+class CustomPositioned extends StatelessWidget {
+  const CustomPositioned({super.key, required this.child, this.size = 100});
+
+  final Widget child;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Column(
+        children: [
+          const Spacer(),
+          SizedBox(
+            height: size,
+            width: size,
+            child: child,
+          ),
+          const Spacer(flex: 2),
+        ],
+      ),
+    );
+  }
+}
